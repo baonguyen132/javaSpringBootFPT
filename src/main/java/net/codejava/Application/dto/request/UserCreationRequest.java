715be3ce3db3d996958b9a74d4025acbe9b828af
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2026-08-19 22:58:22 +0700

    Refactor UserCreationRequest to use Lombok annotations for cleaner code and remove redundant getter/setter methods

--- src/main/java/net/codejava/Application/dto/request/UserCreationRequest.java
+++ src/main/java/net/codejava/Application/dto/request/UserCreationRequest.java
@@ -3,39 +3,24 @@ package net.codejava.Application.dto.request;
 import java.time.LocalDate;
 
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
+@Data
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserCreationRequest {
-    private String name;
+    String name;
 
     @Size(min = 3,max = 50, message = "USER_VALIDATION")
-    private String username ;
+    String username ;
 
     @Size(min = 8,max = 100, message = "PASWORD_VALIDATION")
-    private String password ;
-    private LocalDate dob ;
-
-    public LocalDate getDob() {
-        return dob;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    String password ;
+    LocalDate dob ;
 }
