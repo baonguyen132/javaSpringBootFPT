29ffdd3d0546fedf0f7de8de9da642dd318e5661
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2026-09-25 22:08:41 +0700

    upload

---
+++ src/main/java/net/codejava/Application/configuration/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package net.codejava.Application.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("VR Practice API")
+                        .version("1.0")
+                        .description("REST API for VR Practice Project"))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                )
+                .components(
+                        new Components().addSecuritySchemes(
+                                "bearerAuth",
+                                new SecurityScheme().name("Authorization").type(SecurityScheme.Type.HTTP)
+                                                    .scheme("bearer")
+                                                    .bearerFormat("JWT")
+                        )
+                )
+                ;
+    }
+}
